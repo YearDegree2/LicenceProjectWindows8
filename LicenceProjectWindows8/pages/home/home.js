aa887002bd4da8c12da74ca86584da60a47aea08
nicobascon <nico.bascon@live.fr>
2014-12-01 16:57:28 +0100

    Begin application

--- LicenceProjectWindows8/pages/home/home.js
+++ LicenceProjectWindows8/pages/home/home.js
@@ -5,7 +5,11 @@
         // Cette fonction est appelée chaque fois qu'un utilisateur accède à cette page. Elle
         // remplit les éléments de la page avec les données d'application.
         ready: function (element, options) {
-            // TODO: initialisez la page ici.
+            WinJS.Utilities.query("#connexion").listen("click", clickEventHandlerToConnexion, false);
         }
     });
+
+    function clickEventHandlerToConnexion() {
+        WinJS.Navigation.navigate("/pages/connexion/connexion.html");
+    }
 })();
