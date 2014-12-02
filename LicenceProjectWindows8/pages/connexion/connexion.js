b224e242d7ceb90be931f356d166b0602a4b1b75
yannisrichard <yannisrichard63@gmail.com>
2014-12-02 15:29:03 +0100

    Add event enter for connexion.js

--- LicenceProjectWindows8/pages/connexion/connexion.js
+++ LicenceProjectWindows8/pages/connexion/connexion.js
@@ -10,48 +10,57 @@
             WinJS.UI.processAll().done(function () {
                 // Cette fonction est appelée une fois la page chargé (processAll()).
                 var anchor = document.getElementById("sendFormConnexion");
-                anchor.addEventListener("click", function () {
-                    // Cette fonction est appelée lors d'un click sur
-                    // le bouton envoyer.
-                    var objData = {};
-                    objData.username = document.getElementById("_username").value;
-                    objData.password = document.getElementById("_password").value;
-
-                    var options = {
-                        url: beginAddress + "/login",
-                        type: "POST",
-                        data: JSON.stringify(objData),
-                        headers: { "Content-Type": "application/json;charset=utf-8" },
-                        responseType: 'json'
+                anchor.addEventListener("click", clickEventHandlerToadminHome);
+                document.getElementById("_username").addEventListener("keypress", function (e) {
+                    var key = e.which || e.keyCode;
+                    if (key == 13) {
+                        clickEventHandlerToadminHome();
                     }
-                    WinJS.xhr(options).done(
-                        //success and error handlers
-                        function success(req) {
-                            var hashedKey = req.response;
-                            WinJS.Navigation.navigate(adminHome, { keyaaa: hashedKey });
-                        }
-                        ,
-                        function error(err) {
-                            //var erreur = err.response;
-                            var statusCode = err.status;
-                            if (statusCode != 200) {
-                                Windows.UI.Popups.MessageDialog("Identifiant ou mot de passe invalide").showAsync();
-                            }
-                            return;
-                        }
-                    );
+                });
+                document.getElementById("_password").addEventListener("keypress", function (e) {
+                    var key = e.which || e.keyCode;
+                    if (key == 13) { 
+                        clickEventHandlerToadminHome();
+                    }
                 });
             });
         },
-
         unload: function () {
             // TODO: répondre aux navigations en dehors de cette page.
         },
-
         updateLayout: function (element) {
             /// <param name="element" domElement="true" />
 
             // TODO: répondez aux modifications de la disposition.
         }
     });
+
+    function clickEventHandlerToadminHome () {
+        // Cette fonction est appelée lors d'un click sur
+        // le bouton Envoyer ou sur le bouton Enter.
+        var objData = {};
+        objData.username = document.getElementById("_username").value;
+        objData.password = document.getElementById("_password").value;
+        var options = {
+            url: beginAddress + "/login",
+            type: "POST",
+            data: JSON.stringify(objData),
+            headers: { "Content-Type": "application/json;charset=utf-8" },
+            responseType: 'json'
+        }
+        WinJS.xhr(options).done(
+            function success(req) {
+                var hashedKey = req.response;
+                WinJS.Navigation.navigate(adminHome, { keyaaa: hashedKey });
+            }
+            ,
+            function error(err) {
+                var statusCode = err.status;
+                if (statusCode != 200) {
+                    Windows.UI.Popups.MessageDialog("Identifiant ou mot de passe invalide").showAsync();
+                }
+                return;
+            }
+        );
+    }
 })();
