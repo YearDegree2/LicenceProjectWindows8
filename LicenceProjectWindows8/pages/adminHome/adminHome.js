8e81550de1cac2ff6cb6d9c281fbc1a631a950c5
yannisrichard <yannisrichard63@gmail.com>
2014-12-03 15:42:01 +0100

    Add ListView

--- LicenceProjectWindows8/pages/adminHome/adminHome.js
+++ LicenceProjectWindows8/pages/adminHome/adminHome.js
@@ -11,6 +11,16 @@
             if (WinJS.Navigation.state.key) {
                 hashedKey = WinJS.Navigation.state.key;
             }
+
+            var listView = element.querySelector('#basicListView').winControl;
+            function itemInvokedHandler(eventObject) {
+                eventObject.detail.itemPromise.done(function (invokedItem) {
+                    invokedItem.data.link;
+                    WinJS.Navigation.navigate(invokedItem.data.link);
+                });
+            }
+
+            listView.addEventListener("iteminvoked", itemInvokedHandler, false);
         },
 
         unload: function () {
@@ -23,4 +33,25 @@
             // TODO: répondez aux modifications de la disposition.
         }
     });
+    var dataArray = [
+  { title: "Basic banana", text: "Low-fat frozen yogurt", picture: "../../images/60banana.png", link: "/pages/page1/page1.html" },
+  { title: "Banana blast", text: "Ice cream", picture: "../../images/60banana.png", link: "/pages/page2/page2.html" },
+  { title: "Brilliant citron", text: "Frozen custard", picture: "../../images/60Lemon.png", link: "/pages/page3/page3.html" },
+  { title: "Orange surprise", text: "Sherbet", picture: "../../images/60orange.png", link: "/pages/page1/page1.html" },
+  { title: "Original orange", text: "Sherbet", picture: "../../images/60orange.png", link: "/pages/page1/page1.html" },
+  { title: "Vanilla", text: "Ice cream", picture: "../../images/60vanilla.png", link: "/pages/page1/page1.html" },
+  { title: "Very vanilla", text: "Frozen custard", picture: "../../images/60vanilla.png", link: "/pages/page1/page1.html" },
+  { title: "Marvelous mint", text: "Gelato", picture: "../../images/60mint.png", link: "/pages/page1/page1.html" },
+  { title: "Basic banana", text: "Low-fat frozen yogurt", picture: "../../images/60banana.png", link: "/pages/page1/page1.html" },
+  { title: "Banana blast", text: "Ice cream", picture: "../../images/60banana.png", link: "/pages/page1/page1.html" },
+  { title: "Brilliant citron", text: "Frozen custard", picture: "../../images/60Lemon.png", link: "/pages/page1/page1.html" },
+  { title: "Orange surprise", text: "Sherbet", picture: "../../images/60orange.png", link: "/pages/page1/page1.html" },
+    ];
+
+    var dataList = new WinJS.Binding.List(dataArray);
+    var publicMembers =
+    {
+        itemList: dataList
+    };
+    WinJS.Namespace.define("DataExample", publicMembers);
 })();
---
+++ LicenceProjectWindows8/pages/page1/page1.js
@@ -0,0 +1,23 @@
+﻿// Pour obtenir une présentation du modèle Contrôle de page, consultez la documentation suivante :
+// http://go.microsoft.com/fwlink/?LinkId=232511
+(function () {
+    "use strict";
+
+    WinJS.UI.Pages.define("/pages/page1/page1.html", {
+        // Cette fonction est appelée chaque fois qu'un utilisateur accède à cette page. Elle
+        // remplit les éléments de la page avec les données d'application.
+        ready: function (element, options) {
+            // TODO: initialisez la page ici.
+        },
+
+        unload: function () {
+            // TODO: répondre aux navigations en dehors de cette page.
+        },
+
+        updateLayout: function (element) {
+            /// <param name="element" domElement="true" />
+
+            // TODO: répondez aux modifications de la disposition.
+        }
+    });
+})();
