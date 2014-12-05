eb6597ddd8abbfd91fa41055ba2836f71ffdf66d
yannisrichard <yannisrichard63@gmail.com>
2014-12-05 17:18:30 +0100

    Add updateRubrique

--- LicenceProjectWindows8/pages/rubrique/updateRubrique/updateRubrique.js
+++ LicenceProjectWindows8/pages/rubrique/updateRubrique/updateRubrique.js
@@ -1,23 +1,138 @@
-﻿// Pour obtenir une présentation du modèle Contrôle de page, consultez la documentation suivante :
-// http://go.microsoft.com/fwlink/?LinkId=232511
-(function () {
+﻿(function () {
     "use strict";
 
-    WinJS.UI.Pages.define("/pages/rubrique/updateRubrique/updateRubrique.html", {
-        // Cette fonction est appelée chaque fois qu'un utilisateur accède à cette page. Elle
-        // remplit les éléments de la page avec les données d'application.
+    WinJS.UI.Pages.define(updateRubrique, {
         ready: function (element, options) {
-            // TODO: initialisez la page ici.
-        },
+            var hashedKey = "";
+            if (WinJS.Navigation.state.key) {
+                hashedKey = WinJS.Navigation.state.key;
+            }
+            var idRubrique = "";
+            if (WinJS.Navigation.state.idRubrique) {
+                idRubrique = WinJS.Navigation.state.idRubrique;
+            }
 
-        unload: function () {
-            // TODO: répondre aux navigations en dehors de cette page.
-        },
+            var objData = {};
+            var options = {
+                url: beginAddress + "/rubriques/" + idRubrique,
+                type: "GET",
+                data: JSON.stringify(objData),
+                headers: { "Content-Type": "application/json;charset=utf-8" },
+                responseType: 'json'
+            }
+            WinJS.xhr(options).done(
+                function success(result) {
+                    var jsonDocument = result.responseText;
+                    var rubrique = JSON.parse(jsonDocument);
+                    var titre_fr = document.getElementById("titre_fr");
+                    titre_fr.value += rubrique.titre_fr;
+                    var titre_en = document.getElementById("titre_en");
+                    titre_en.value += rubrique.titre_en;
+                    var content_fr = document.getElementById("content_fr");
+                    content_fr.value += rubrique.content_fr;
+                    var content_en = document.getElementById("content_en");
+                    content_en.value += rubrique.content_en;
+                    var actif = document.getElementById("actif");
+                    actif.value += rubrique.actif;
+                    var position = document.getElementById("position");
+                    position.value += rubrique.position;
+                    var sendRubrique = document.getElementById("sendRubrique");
+                    sendRubrique.addEventListener("click", function () {
+                        clickEventHandlerToUpdateRubrique(idRubrique, hashedKey);
+                    });
+                    document.getElementById("titre_fr").addEventListener("keypress", function (e) {
+                        var key = e.which || e.keyCode;
+                        if (key == 13) {
+                            clickEventHandlerToUpdateRubrique(idRubrique, hashedKey);
+                        }
+                    });
+                    document.getElementById("titre_en").addEventListener("keypress", function (e) {
+                        var key = e.which || e.keyCode;
+                        if (key == 13) {
+                            clickEventHandlerToUpdateRubrique(idRubrique, hashedKey);
+                        }
+                    });
+                    document.getElementById("content_fr").addEventListener("keypress", function (e) {
+                        var key = e.which || e.keyCode;
+                        if (key == 13) {
+                            clickEventHandlerToUpdateRubrique(idRubrique, hashedKey);
+                        }
+                    });
+                    document.getElementById("content_en").addEventListener("keypress", function (e) {
+                        var key = e.which || e.keyCode;
+                        if (key == 13) {
+                            clickEventHandlerToUpdateRubrique(idRubrique, hashedKey);
+                        }
+                    });
+                    document.getElementById("actif").addEventListener("keypress", function (e) {
+                        var key = e.which || e.keyCode;
+                        if (key == 13) {
+                            clickEventHandlerToUpdateRubrique(idRubrique, hashedKey);
+                        }
+                    });
+                    document.getElementById("position").addEventListener("keypress", function (e) {
+                        var key = e.which || e.keyCode;
+                        if (key == 13) {
+                            clickEventHandlerToUpdateRubrique(idRubrique, hashedKey);
+                        }
+                    });
 
-        updateLayout: function (element) {
-            /// <param name="element" domElement="true" />
+                },
 
-            // TODO: répondez aux modifications de la disposition.
+                function error(err) {
+                    if (400 === err.status) {
+                        Windows.UI.Popups.MessageDialog("La rubrique n'existe pas").showAsync();
+                        WinJS.Navigation.back();
+                        return;
+                    }
+                    Windows.UI.Popups.MessageDialog("Une erreur s'est produite.").showAsync();
+                    WinJS.Navigation.back();
+                }
+            );
         }
     });
+
+    function clickEventHandlerToUpdateRubrique(idRubrique, hashedKey) {
+        var objData = {};
+        objData.a = hashedKey;
+        if (document.getElementById("titre_fr").value != "") {
+            objData.titre_fr = document.getElementById("titre_fr").value;
+        }
+        if (document.getElementById("titre_en").value != "") {
+            objData.titre_en = document.getElementById("titre_en").value;
+        }
+        objData.actif = document.getElementById("actif").value;
+        objData.position = document.getElementById("position").value;
+        objData.content_fr = document.getElementById("content_fr").value;
+        objData.content_en = document.getElementById("content_en").value;
+        var options = {
+            url: beginAddress + "/admin/rubriques/" + idRubrique,
+            type: "PUT",
+            data: JSON.stringify(objData),
+            headers: { "Content-Type": "application/json;charset=utf-8" },
+            responseType: 'json'
+        }
+        WinJS.xhr(options).done(
+            function success(result) {
+                if (200 === result.status) {
+                    Windows.UI.Popups.MessageDialog("Rubrique modifiée").showAsync();
+                    WinJS.Navigation.navigate(adminHome, { key: hashedKey });
+                }
+            },
+
+            function error(err) {
+                if (400 === err.status) {
+                    Windows.UI.Popups.MessageDialog("La rubrique n'existe pas").showAsync();
+                    WinJS.Navigation.back();
+                    return;
+                }
+                if (404 === err.status) {
+                    Windows.UI.Popups.MessageDialog("Attributs manquants").showAsync();
+                    return;
+                }
+                Windows.UI.Popups.MessageDialog("Une erreur s'est produite.").showAsync();
+                WinJS.Navigation.navigate(adminHome, { key: hashedKey });
+            }
+        );
+    }
 })();
