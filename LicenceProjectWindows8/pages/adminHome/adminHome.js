95aae108cfa315f7cd7a51d0f52c4f7734b50f91
nicobascon <nico.bascon@live.fr>
2014-12-08 10:18:31 +0100

    Suppress subtitles on ListView

--- LicenceProjectWindows8/pages/adminHome/adminHome.js
+++ LicenceProjectWindows8/pages/adminHome/adminHome.js
@@ -20,12 +20,12 @@
     });
 
     var dataArray = [
-        { title: "Ajouter une catégorie", text: "Ajouter une catégorie", picture: "/images/60banana.png", link: addCategorie },
-        { title: "Lister les catégories", text: "Lister les catégories", picture: "/images/60banana.png", link: getCategories },
-        { title: "Ajouter une publication", text: "Ajouter une publication", picture: "/images/60Lemon.png", link: addPublication },
-        { title: "Lister les publications", text: "Lister les publications", picture: "/images/60Lemon.png", link: getPublications },
-        { title: "Ajouter une rubrique", text: "Ajouter une rubrique", picture: "/images/60vanilla.png", link: addRubrique },
-        { title: "Lister les rubriques", text: "Lister les rubriques", picture: "/images/60vanilla.png", link: getRubriques },
+        { title: "Ajouter une catégorie", picture: "/images/60banana.png", link: addCategorie },
+        { title: "Lister les catégories", picture: "/images/60banana.png", link: getCategories },
+        { title: "Ajouter une publication", picture: "/images/60Lemon.png", link: addPublication },
+        { title: "Lister les publications", picture: "/images/60Lemon.png", link: getPublications },
+        { title: "Ajouter une rubrique", picture: "/images/60vanilla.png", link: addRubrique },
+        { title: "Lister les rubriques", picture: "/images/60vanilla.png", link: getRubriques },
     ];
 
     var dataList = new WinJS.Binding.List(dataArray);
