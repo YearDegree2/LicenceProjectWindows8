dc97581fa124ca8602478bbca1533b5887c9be1e
nicobascon <nico.bascon@live.fr>
2014-12-08 10:12:58 +0100

    Validate appli

--- LicenceProjectWindows8/pages/adminHome/adminHome.js
+++ LicenceProjectWindows8/pages/adminHome/adminHome.js
@@ -24,18 +24,8 @@
         { title: "Lister les catégories", text: "Lister les catégories", picture: "/images/60banana.png", link: getCategories },
         { title: "Ajouter une publication", text: "Ajouter une publication", picture: "/images/60Lemon.png", link: addPublication },
         { title: "Lister les publications", text: "Lister les publications", picture: "/images/60Lemon.png", link: getPublications },
-
-        { title: "Brilliant citron", text: "Frozen custard", picture: "/images/60Lemon.png", link: "/pages/page3/page3.html" },
-        { title: "Orange surprise", text: "Sherbet", picture: "/images/60orange.png", link: "/pages/page1/page1.html" },
         { title: "Ajouter une rubrique", text: "Ajouter une rubrique", picture: "/images/60vanilla.png", link: addRubrique },
         { title: "Lister les rubriques", text: "Lister les rubriques", picture: "/images/60vanilla.png", link: getRubriques },
-
-        { title: "Very vanilla", text: "Frozen custard", picture: "/images/60vanilla.png", link: "/pages/page1/page1.html" },
-        { title: "Marvelous mint", text: "Gelato", picture: "/images/60mint.png", link: "/pages/page1/page1.html" },
-        { title: "Basic banana", text: "Low-fat frozen yogurt", picture: "/images/60banana.png", link: "/pages/page1/page1.html" },
-        { title: "Banana blast", text: "Ice cream", picture: "/images/60banana.png", link: "/pages/page1/page1.html" },
-        { title: "Brilliant citron", text: "Frozen custard", picture: "/images/60Lemon.png", link: "/pages/page1/page1.html" },
-        { title: "Orange surprise", text: "Sherbet", picture: "/images/60orange.png", link: "/pages/page1/page1.html" },
     ];
 
     var dataList = new WinJS.Binding.List(dataArray);
